b9f28fe0ac5fae67fc289779404f108c2fd6403e
Nick Rempel <nick@lucent.is>
2020-07-21 16:23:13 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 set -e
 
-# For some reason, $GITHUB_REF wasn't working before. This is a good alternative.
-# REF=$(jq -r ".ref" "$GITHUB_EVENT_PATH")
-# echo "Ref from JSON: $REF"
-
-if ! git status > /dev/null 2>&1
-then
-  echo "## Initializing git repo..."
-  git init
-fi
-
 REMOTE_TOKEN_URL="https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 if ! git remote | grep "origin" > /dev/null 2>&1
 then 
@@ -22,12 +12,11 @@ fi
 
 git remote set-url --push origin $REMOTE_TOKEN_URL
 
-BRANCH="$GITHUB_HEAD_REF"
+BRANCH="$GITHUB_REF"
 echo "### Branch: $BRANCH"
 git checkout $BRANCH
 
 echo "## Login into git..."
-git config --global user.email "black_code_formatter@gmail.com"
 git config --global user.name "Black Code Formatter"
 
 echo "## Running Black Code Formatter"
